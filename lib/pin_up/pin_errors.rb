d6b48df34dc1c33daab556d52032148c4a543d4c
Daniel Nitsikopoulos <dnitza@gmail.com>
2013-07-08 19:21:03 +0300

    added InsufficientPinBalance error

--- lib/pin_up/pin_errors.rb
+++ lib/pin_up/pin_errors.rb
@@ -15,6 +15,8 @@ module Pin
       case http_status
       when 400
         raise(Pin::ChargeError, "#{response['error']}: #{response['error_description']}. Charge token: #{response['charge_token']}")
+      when 402
+        raise(Pin::InsufficientPinBalance, "#{response['error_description']}")
       when 404
         raise(Pin::ResourceNotFound, "#{response['error_description']}")
       when 422
@@ -34,6 +36,9 @@ module Pin
   end
 
   class ChargeError < PinError
+  end
+
+  class InsufficientPinBalance <PinError
   end
 
 end
