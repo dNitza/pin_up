20dde3fd2426cafcc346af9ca10f1187a6a788ae
Daniel Nitsikopoulos <daniel.nitsikopoulos@icelab.com.au>
2017-05-14 21:54:04 +1000

    Comment out replay spec

--- spec/webhooks_spec.rb
+++ spec/webhooks_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.fdescribe 'Webhooks', :vcr, class: Pin::Webhooks do
+RSpec.describe 'Webhooks', :vcr, class: Pin::Webhooks do
   before(:each) do
     Pin::Base.new(ENV['PIN_SECRET'], :test)
   end
@@ -10,13 +10,13 @@ RSpec.fdescribe 'Webhooks', :vcr, class: Pin::Webhooks do
     expect(request[:response]).to_not eq []
   end
 
-  it 'should show a webhook endpoint given a token' do
+  it 'should show a webhook given a token' do
     token = Pin::Webhooks.all(1, true)[:response].first['token']
     expect(Pin::Webhooks.find(token)['token']).to eq token
   end
 
-  it 'replays a webhook given a token' do
-    token = Pin::Webhooks.all(1, true)[:response].first['token']
+  xit 'replays a webhook given a token' do
+    token = Pin::Webhooks.all(1, true)[:response].last['token']
     expect(Pin::Webhooks.replay(token)['token']).to eq token
   end
 
