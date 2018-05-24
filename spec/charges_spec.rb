e890769d2f548fd9423c343410d10fa2833def74
Andre Vidic <andre@dtcorp.com.au>
2018-05-24 18:31:22 +1000

    Decouple test from user API data & update readme

--- spec/charges_spec.rb
+++ spec/charges_spec.rb
@@ -1,8 +1,58 @@
 require 'spec_helper'
 
 describe 'Charge', :vcr, class: Pin::Charges do
+  let(:card_1) {
+    { number: '5520000000000000',
+      expiry_month: '12',
+      expiry_year: '2025',
+      cvc: '123',
+      name: 'Roland Robot',
+      address_line1: '123 Fake Street',
+      address_city: 'Melbourne',
+      address_postcode: '1234',
+      address_state: 'Vic',
+      address_country: 'Australia' }
+  }
+
+  let(:customer_token) {
+    Pin::Customer.create('email@example.com', card_1)['token']
+  }
+
+  let(:charge) {
+    Pin::Charges.create(email: 'email@example.com',
+                        description: 'Charge description',
+                        amount: '500',
+                        currency: 'AUD',
+                        number: '5520000000000000',
+                        ip_address: '203.192.1.172',
+                        customer_token: customer_token)
+  }
+
+  let(:charge_hash) {
+    { email: 'email@example.com',
+      description: 'A new charge from testing Pin gem',
+      amount: '400',
+      currency: 'AUD',
+      ip_address: '127.0.0.1',
+      customer_token: customer_token }
+  }
+
+  let(:charge_capture_false) {
+    { email: 'email@example.com',
+      description: 'A new captured charge from testing Pin gem',
+      amount: '400',
+      currency: 'AUD',
+      ip_address: '127.0.0.1',
+      customer_token: customer_token,
+      capture: false }
+  }
+
   before(:each) do
     Pin::Base.new(ENV['PIN_SECRET'], :test)
+    # Create a customer
+    customer_token
+    # Create a charge
+    charge
   end
 
   it 'should list charges in Pin' do
@@ -14,17 +64,15 @@ describe 'Charge', :vcr, class: Pin::Charges do
   end
 
   it 'should create a charge given details' do
-    customer = Pin::Customer.create('email@example.com', number: '5520000000000000', expiry_month: '12', expiry_year: Time.now.year+1, cvc: '123', name: 'Roland Robot', address_line1: '123 fake street', address_city: 'Melbourne', address_postcode: '1234', address_state: 'Vic', address_country: 'Australia')
-    options = { email: 'email@example.com', description: 'A new charge from testing Pin gem', amount: '400', currency: 'AUD', ip_address: '127.0.0.1', customer_token: customer['token'] }
-    expect(Pin::Charges.create(options)['success']).to eq true
+    expect(Pin::Charges.create(charge_hash)['success']).to eq true
   end
 
   it 'should show a charge given a token' do
-    expect(Pin::Charges.find('ch_YFEgBSs5qTIWggGt72jn7Q')['token']).to match(/^[a-z]{2}[_]/)
+    expect(Pin::Charges.find(charge['token'])['token']).to match(/^[a-z]{2}[_]/)
   end
 
   it 'should show a charge given a search term' do
-    expect(Pin::Charges.search(query: 'A new charge from testing Pin gem', end_date: 'Aug 31, 2016')).to_not eq []
+    expect(Pin::Charges.search(query: 'Charge Desc', end_date: 'Aug 31, 2025')).to_not eq []
   end
 
   it 'should return pagination if "pagination" is true' do
@@ -36,23 +84,19 @@ describe 'Charge', :vcr, class: Pin::Charges do
   end
 
   it 'should return pagination for search if "pagination" is true' do
-    expect(Pin::Charges.search(3, true, query: 'A new charge from testing Pin gem', end_date: 'Aug 31, 2016')[:pagination]['current']).to eq 3
+    expect(Pin::Charges.search(3, true, query: 'Charge Desc', end_date: 'Aug 31, 2025')[:pagination]['current']).to eq 3
   end
 
   it 'should list charges for search on a page given a page' do
-    expect(Pin::Charges.search(1, query: 'A new charge from testing Pin gem', end_date: 'Aug 31, 2016')).to_not eq []
+    expect(Pin::Charges.search(1, query: 'Charge Desc', end_date: 'Aug 31, 2025')).to_not eq []
   end
 
   it 'should create a pre-auth (capture a charge)' do
-    customer = Pin::Customer.create('email@example.com', number: '5520000000000000', expiry_month: '12', expiry_year: Time.now.year+1, cvc: '123', name: 'Roland Robot', address_line1: '123 fake street', address_city: 'Melbourne', address_postcode: '1234', address_state: 'Vic', address_country: 'Australia')
-    options = { email: 'email@example.com', description: 'A new captured charge from testing Pin gem', amount: '400', currency: 'AUD', ip_address: '127.0.0.1', customer_token: customer['token'], capture: false }
-    expect(Pin::Charges.create(options)['captured']).to eq false
+    expect(Pin::Charges.create(charge_capture_false)['captured']).to eq false
   end
 
   it 'should capture a charge' do
-    customer = Pin::Customer.create('email@example.com', number: '5520000000000000', expiry_month: '12', expiry_year: Time.now.year+1, cvc: '123', name: 'Roland Robot', address_line1: '123 fake street', address_city: 'Melbourne', address_postcode: '1234', address_state: 'Vic', address_country: 'Australia')
-    options = { email: 'email@example.com', description: 'A new captured charge from testing Pin gem', amount: '400', currency: 'AUD', ip_address: '127.0.0.1', customer_token: customer['token'], capture: false }
-    token = Pin::Charges.create(options)['token']
+    token = Pin::Charges.create(charge_capture_false)['token']
     expect(Pin::Charges.capture(token)['success']).to eq true
   end
 end
--- spec/refund_spec.rb
+++ spec/refund_spec.rb
@@ -1,27 +1,61 @@
 require 'spec_helper'
 
 describe 'Refund', :vcr, class: Pin::Refund do
+  let(:card_1) {
+    { number: '5520000000000000',
+     expiry_month: '12',
+     expiry_year: '2025',
+     cvc: '123',
+     name: 'Roland Robot',
+     address_line1: '123 Fake Street',
+     address_city: 'Melbourne',
+     address_postcode: '1234',
+     address_state: 'Vic',
+     address_country: 'Australia' }
+  }
+
+  let(:customer_token) {
+    Pin::Customer.create('email@example.com', card_1)['token']
+  }
+
+  let(:charge) {
+    Pin::Charges.create(email: 'email@example.com',
+                        description: 'Charge description',
+                        amount: '500',
+                        currency: 'AUD',
+                        number: '5520000000000000',
+                        ip_address: '203.192.1.172',
+                        customer_token: customer_token)
+  }
+
+  let(:no_refund_charge) {
+    Pin::Charges.create(email: 'email@example.com',
+                        description: 'Charge description',
+                        amount: '500',
+                        currency: 'AUD',
+                        number: '5520000000000000',
+                        ip_address: '203.192.1.172',
+                        customer_token: customer_token)
+  }
+
   before(:each) do
     Pin::Base.new(ENV['PIN_SECRET'], :test)
+    Pin::Refund.create(charge['token'], '400')
   end
 
   it 'should list all refunds made to a charge given a token' do
-    expect(Pin::Refund.find('ch_5q0DiDnHZIggDfVDqcP-jQ')[0]['token']).to match(/^[a-z]{2}[_]/)
+    expect(Pin::Refund.find(charge['token'])[0]['token']).to match(/^[a-z]{2}[_]/)
   end
 
   it 'should return nothing if looking for a charge without a refund' do
-    expect(Pin::Refund.find('ch_Dsd62Ey5Hmd3B1dDHNKYvA')).to eq []
+    expect(Pin::Refund.find(no_refund_charge['token'])).to eq []
   end
 
   it 'should return a page of refunds given a page and token' do
-    expect(Pin::Refund.find('ch_5q0DiDnHZIggDfVDqcP-jQ', 1, true)[:response]).to_not eq []
+    expect(Pin::Refund.find(charge['token'], 1, true)[:response]).to_not eq []
   end
 
   it 'should create a refund for a given amount and charge' do
-    customer = Pin::Customer.create('email@example.com', number: '5520000000000000', expiry_month: '12', expiry_year: Time.now.year+1, cvc: '123', name: 'Roland Robot', address_line1: '123 fake street', address_city: 'Melbourne', address_postcode: '1234', address_state: 'Vic', address_country: 'Australia')
-    options = { email: 'email@example.com', description: 'A new charge from testing Pin gem', amount: '400', currency: 'AUD', ip_address: '127.0.0.1', customer_token: customer['token']}
-    @charge = Pin::Charges.create(options)
-    expect(Pin::Refund.create(@charge['token'], '400')['amount']).to eq 400
+    expect(Pin::Refund.find(charge['token'])[0]['amount']).to eq 400
   end
-
 end
