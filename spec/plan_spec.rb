7fd39b79d346198ab99b49f531e19eb89d42addb
Andre Vidic <andre@dtcorp.com.au>
2018-05-30 15:49:50 +1000

    Plan-TEST-Plans must be uniquely named

--- spec/plan_spec.rb
+++ spec/plan_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe 'Plan', :vcr, class: Pin::Plan do
+  let(:time) { Time.now.iso8601(4) }
   let(:plan) {
-    { name: 'Coffee Plan',
+    { name: "Plan#{time}",
       amount: '1000',
       currency: 'AUD',
       interval: 30,
@@ -18,7 +19,21 @@ describe 'Plan', :vcr, class: Pin::Plan do
   end
 
   it 'should create a new plan and return its details' do
-    expect(Pin::Plan.create(plan)['token']).to match(/(plan)[_]([\w-]{22})/)
+    expect(Pin::Plan.create(plan))
+      .to match a_hash_including("name"=>"Plan#{time}",
+                                 "token"=>match(/(plan)[_]([\w-]{22})/),
+                                 "amount"=>1000,
+                                 "currency"=>"AUD",
+                                 "setup_amount"=>0,
+                                 "trial_amount"=>0,
+                                 "interval"=>30,
+                                 "interval_unit"=>"day",
+                                 "trial_interval"=>7,
+                                 "trial_interval_unit"=>"day",
+                                 "expiration_interval"=>0,
+                                 "expiration_interval_unit"=>"",
+                                 "active_subscriptions"=>0,
+                                 "trial_subscriptions"=>0)
   end
 
   it 'should return a paginated list of all plans' do
