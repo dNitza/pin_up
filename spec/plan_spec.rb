6a9450d1ff5dc00d78724209c71bc2ae7b151d63
Andre Vidic <andre@dtcorp.com.au>
2018-05-31 16:01:17 +1000

    Plan-TEST-Create new plan & linked subscription using billing card_2

--- spec/plan_spec.rb
+++ spec/plan_spec.rb
@@ -14,6 +14,22 @@ describe 'Plan', :vcr, class: Pin::Plan do
       trial_interval_unit: 'day' }
   }
 
+  let(:plan_2) {
+    { name: "Subscription#{time}",
+     amount: '1000',
+     currency: 'AUD',
+     interval: 30,
+     interval_unit: 'day',
+     setup_amount: 27900,
+     trial_amount: 0,
+     trial_interval: '',
+     trial_interval_unit: '' }
+  }
+
+  let(:plan_2_token) {
+    Pin::Plan.create(plan_2)['token']
+  }
+
   let(:plan_token) {
     Pin::Plan.create(plan)['token']
   }
@@ -31,6 +47,20 @@ describe 'Plan', :vcr, class: Pin::Plan do
      address_country: 'Australia' }
   }
 
+  let(:card_2) {
+    { number: '4200000000000000',
+     expiry_month: '12',
+     expiry_year: '2020',
+     cvc: '111',
+     name: 'Roland TestRobot',
+     address_line1: '123 Fake Road',
+     address_line2: '',
+     address_city: 'Melbourne',
+     address_postcode: '1223',
+     address_state: 'Vic',
+     address_country: 'AU' }
+  }
+
   let(:customer) {
     Pin::Customer.create('email@example.com', card_1)
   }
@@ -39,15 +69,20 @@ describe 'Plan', :vcr, class: Pin::Plan do
     customer['token']
   }
 
+  let(:card_2_token) {
+    Pin::Card.create(card_2)['token']
+  }
+
+  let(:link_card_2_to_customer) {
+    Pin::Customer.create_card(customer_token, card_2_token)
+  }
+
   let(:subscription) {
     { plan_token: plan_token,
       customer_token: customer_token,
       include_setup_fee: false }
   }
 
-  let(:subscription_token) {
-    Pin::Subscription.create(subscription)['token']
-  }
   before(:each) do
     Pin::Base.new(ENV['PIN_SECRET'], :test)
   end
@@ -109,6 +144,34 @@ describe 'Plan', :vcr, class: Pin::Plan do
   end
 
   it 'should create a new subscription to the specified plan' do
+    plan = { object: Pin::Plan.create_subscription(plan_token, customer_token),
+             created_at: Time.now }
+    expect(plan[:object])
+      .to match a_hash_including("state"=>"trial",
+                                 "trial_started_at"=>"#{plan[:created_at].utc.iso8601}",
+                                 "trial_ended_at"=> nil,
+                                 "next_billing_date"=>"#{(plan[:created_at] + 7*24*60*60).utc.iso8601}",
+                                 "current_period_started_at"=>"#{plan[:created_at].utc.iso8601}",
+                                 "current_period_ends_at"=>"#{(plan[:created_at] + 7*24*60*60).utc.iso8601}",
+                                 "cancelled_at"=>nil,
+                                 "created_at"=>"#{plan[:created_at].utc.iso8601}",
+                                 "updated_at"=>"#{plan[:created_at].utc.iso8601}",
+                                 "token"=>match(/(sub)[_]([\w-]{22})/),
+                                 "plan_token"=>"#{plan_token}",
+                                 "customer_token"=>"#{customer_token}",
+                                 "card_token"=> nil)
+  end
+
+  it 'should create new plan & linked subscription using billing card_2' do
+    link_card_2_to_customer
+    expect(Pin::Plan.create_subscription(plan_2_token, customer_token, card_2_token))
+      .to match a_hash_including("state"=>"active",
+                                 "token"=>match(/(sub)[_]([\w-]{22})/),
+                                 "plan_token"=>"#{plan_2_token}",
+                                 "customer_token"=>"#{customer_token}",
+                                 "card_token"=>"#{card_2_token}")
+    charges = Pin::Customer.charges(customer_token)
+    expect(charges[0]['card']['token']).to eq(card_2_token)
   end
 
   it 'should return a paginated list of subscriptions for a plan' do
