7e78d7b7126f9b7921d86c54d7b02290865d5b88
Andre Vidic <andre@dtcorp.com.au>
2018-06-04 13:16:53 +1000

    Plan-TEST-Remove Subscription reliant Plan testss

--- spec/plan_spec.rb
+++ spec/plan_spec.rb
@@ -77,26 +77,6 @@ describe 'Plan', :vcr, class: Pin::Plan do
     Pin::Customer.create_card(customer_token, card_2_token)
   }
 
-  let(:subscription) {
-    { plan_token: plan_token,
-      customer_token: customer_token,
-      include_setup_fee: false }
-  }
-
-  let(:subscription_token) {
-    Pin::Subscription.create(subscription)['token']
-  }
-
-  let(:subscription_2) {
-    { plan_token: plan_2_token,
-     customer_token: customer_token,
-     include_setup_fee: false }
-  }
-
-  let(:subscription_2_token) {
-    Pin::Subscription.create(subscription_2)['token']
-  }
-
   before(:each) do
     Pin::Base.new(ENV['PIN_SECRET'], :test)
   end
@@ -150,40 +130,5 @@ describe 'Plan', :vcr, class: Pin::Plan do
 
   it 'should delete a plan with zero subscriptions given a token' do
     expect(Pin::Plan.delete(plan_token).code).to eq 204
-  end
-
-  it 'should delete a plan and all de-activated subscriptions' do
-    subscription_2_token # attach a subscription to the plan
-    Pin::Subscription.delete(subscription_2_token) # deactivate subscription
-    expect(Pin::Plan.delete(plan_token).code).to eq 204 
-  end
-
-  it 'should create a new subscription to the specified plan' do
-    plan = { object: Pin::Plan.create_subscription(plan_token, customer_token),
-             created_at: Time.now }
-    expect(plan[:object])
-      .to match a_hash_including("state"=>"trial",
-                                 "token"=>match(/(sub)[_]([\w-]{22})/),
-                                 "plan_token"=>"#{plan_token}",
-                                 "customer_token"=>"#{customer_token}",
-                                 "card_token"=> nil)
-  end
-
-  it 'should create new plan & linked subscription using billing card_2' do
-    link_card_2_to_customer
-    expect(Pin::Plan.create_subscription(plan_2_token, customer_token, card_2_token))
-      .to match a_hash_including("state"=>"active",
-                                 "token"=>match(/(sub)[_]([\w-]{22})/),
-                                 "plan_token"=>"#{plan_2_token}",
-                                 "customer_token"=>"#{customer_token}",
-                                 "card_token"=>"#{card_2_token}")
-    charges = Pin::Customer.charges(customer_token)
-    expect(charges[0]['card']['token']).to eq(card_2_token)
-  end
-
-  it 'should return a paginated list of subscriptions for a plan' do
-    subscription_token # attach a subscription to the plan
-    subscription_list = Pin::Plan.subscriptions(plan_token)
-    expect(subscription_list[0]['token']).to eq(subscription_token)
   end
 end
